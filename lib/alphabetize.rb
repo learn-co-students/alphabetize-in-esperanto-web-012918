313fa79c1e531843c33e6cc6460e97a3f01cde00
Ryan Farney <ryanfarney@Ryans-MacBook-Pro-104.local>
2018-01-19 11:33:05 -0800

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |word| 
+    word.chars.map { |letter| ESPERANTO_ALPHABET.index(letter)}
+end
 end
