0dd39f1b4bbc75a6fb260ccee864022f7431812f
jaf7 <janthonyfields@gmail.com>
2018-01-21 16:51:24 -0500

    Created Esperanto alphabetizer

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by { |w| w.split('').collect {|l| alphabet.index(l)} }
+  # Use the index positon in the alphabet string:
+  # for each word: split the word to obtain an array of letters
+  # call Array#collect (map) on that array of letters
+  # yield (receive? yield to?) a numeric value, the index position in "alphabet"
+  # index position values (integers) are compared (implicit <=>?)
 end
