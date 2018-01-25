3d41271a70a6f9eee31a16fc6eed0707962b0ad1
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-25 16:34:09 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+
+  arr.sort do |string1, string2|
+    x = "string"
+    inc = 0
+    until x != "string"
+        if string1[inc] != " " || string2[inc] != " "
+         if eSPERANTO_ALPHABET.index(string1[inc]) < eSPERANTO_ALPHABET.index(string2[inc])
+            # binding.pry
+           x = -1
+         elsif  eSPERANTO_ALPHABET.index(string1[inc]) > eSPERANTO_ALPHABET.index(string2[inc])
+           x = 1
+         end
+       end
+       inc +=1
+     end
+     x
+   end
 end
